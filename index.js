8196ed441796c35b50b4efa7e61e136758a3c890
Mike Hirth <hirthy@gmail.com>
2015-03-29 12:00:40 -0400

    Change redis variables

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var path    = require('path');
 
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  var redis = require("redis");
+  var client = redis.createClient(rtg.port, rtg.hostname);
   redis.auth(rtg.auth.split(":")[1]);
 } else {
   var redis = require("redis")
